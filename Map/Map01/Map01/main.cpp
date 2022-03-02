e8de4a9bb4b74367fa56586b59ba20ff7d21eca0
nootitle <nightcity6348@gmail.com>
2022-03-02 10:53:51 +0900

    0302

--- Map/Map01/Map01/main.cpp
+++ Map/Map01/Map01/main.cpp
@@ -15,42 +15,71 @@ using namespace std;
 
 class Object
 {
-
+public:
+	string Name;
+	virtual int GetNumber() = 0;
+	Object() {}
+	Object(string _name) : Name(_name){} //초기화 형태를 잡아줌(string 값으로 초기화)
+	Object(string _name, int i) { Name =  "Enemy" + to_string(i); } //초기화 형태를 잡아줌(string 값으로 초기화)
+	~Object() {}
 };
 
 class Enemy : public Object
 {
 public:
 	int iNumber;
+	virtual int GetNumber() override
+	{
+		return iNumber;
+	}
+
+public:
 	Enemy() {}
+	Enemy(string _name) : Object(_name) {} //구체적인 초기화 모델 확립
+	Enemy(string _name, int i) { Name = "Enemy" + to_string(i); }
 	Enemy(int _i) : iNumber(_i) {}
 };
 
 int main()
 {
+	/*
 	map<string, list<Enemy*>> Objects;
 	list<Enemy*> Enemys;
 
-	Enemys.push_back(new Enemy(1));
-	Enemys.push_back(new Enemy(2));
-	Enemys.push_back(new Enemy(3));
+	for(int i = 0; i < 100; ++i)
+	Enemys.push_back(new Enemy(i));
+
 	Objects["Enemy"] = Enemys;
-	
+	*/
 	//*** 출력 답안 ***
-	//답안1 : 깊은 복사 버전
+	//답안1
+	/*
 	auto temp = Objects["Enemy"];
 	while (!temp.empty())
 	{
 		cout << temp.front()->iNumber << endl;
 		temp.pop_front();
 	}
-
-	//답안2 : iterator 버전
+	*/
+	//답안2
+	/*
 	for (auto itr = Objects["Enemy"].begin(); itr != Objects["Enemy"].end(); ++itr)
 	{
 		auto temp2 = *itr;
 		cout << temp2->iNumber << endl;
 	}
-	
+	*/
+
+	map<int, Object*> Objects;
+
+	for (int i = 0; i < 100; ++i)
+		Objects.insert(make_pair(i, new Enemy("Enemy", i)));
+
+	for (auto itr = Objects.begin(); itr != Objects.end(); ++itr)
+		cout << itr->second->Name << endl;
+
+	auto itr = Objects.find(0);
+	cout << itr->second->Name << endl;
+
 	return 0;
 }
